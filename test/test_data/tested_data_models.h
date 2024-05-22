f0b88c55b8676eb35b6e9799ac3bb6614b3a8d1d
Silas Waxter <silaswaxter@gmail.com>
2024-05-22 15:47:52 -0700

    test: add unique padding 32 to 16 bit data model tests

--- test/test_data/tested_data_models.h
+++ test/test_data/tested_data_models.h
@@ -27,6 +27,17 @@ using Simple32BitDataModel =
                                      4, 4, 8, 4, 8, 4,
                                      1, 1>;
 
+// Model is based on TI's MSP430s; see Data Type Storage, section 6.2.1,
+// (here: https://www.ti.com/lit/ug/slau132r/slau132r.pdf)
+using Simple16BitDataModel =
+    little_pp::data_model::DataModel<little_pp::data_model::Endianess::kLittleEndian,
+                                     1, 1, 1, 1, 1, 1, 2, 2,
+                                     2, 2, 2, 2,
+                                     4, 2, 4, 2,
+                                     8, 2, 8, 2,
+                                     8, 2, 8, 2,
+                                     4, 2, 8, 2, 8, 2,
+                                     1, 1>;
 // NOLINTEND(*-magic-numbers)
 // clang-format on
 }  // namespace data_models
--- test/unique_padding_test.cc
+++ test/unique_padding_test.cc
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
 
 #include "include/little_pp.h"
-#include "std_array_comparison_operators.h"
 #include "test_data/tested_data_models.h"
 
 // alias gtest macro so that its clear why there is commented-out code
@@ -29,14 +28,17 @@ TEST(UniquePadding, Std32BitDataModelToNoPaddingDataModel) {
 
   EXPECT_EQ(kExpected.size(), kGot.size());
   for (std::size_t i = 0; i < kExpected.size(); i++) {
-    EXPECT_EQ(kExpected[i].padding_first_index, kGot[i].padding_first_index);
-    EXPECT_EQ(kExpected[i].padding_span_length, kGot[i].padding_span_length);
+    EXPECT_EQ(kExpected[i].padding_first_index, kGot[i].padding_first_index)
+        << "i = " << i;
+    EXPECT_EQ(kExpected[i].padding_span_length, kGot[i].padding_span_length)
+        << "i = " << i;
     EXPECT_EQ(kExpected[i].is_padding_for_first_passed_data_model,
-              kGot[i].is_padding_for_first_passed_data_model);
+              kGot[i].is_padding_for_first_passed_data_model)
+        << "i = " << i;
   }
 }
 
-TEST(UniquePadding, PaddingDataModelToStd32BitDataModel) {
+TEST(UniquePadding, NoPaddingDataModelToStd32BitDataModel) {
   struct TestedStructType {
     char a;
     int b;
@@ -58,9 +60,44 @@ TEST(UniquePadding, PaddingDataModelToStd32BitDataModel) {
 
   EXPECT_EQ(kExpected.size(), kGot.size());
   for (std::size_t i = 0; i < kExpected.size(); i++) {
-    EXPECT_EQ(kExpected[i].padding_first_index, kGot[i].padding_first_index);
-    EXPECT_EQ(kExpected[i].padding_span_length, kGot[i].padding_span_length);
+    EXPECT_EQ(kExpected[i].padding_first_index, kGot[i].padding_first_index)
+        << "i = " << i;
+    EXPECT_EQ(kExpected[i].padding_span_length, kGot[i].padding_span_length)
+        << "i = " << i;
     EXPECT_EQ(kExpected[i].is_padding_for_first_passed_data_model,
-              kGot[i].is_padding_for_first_passed_data_model);
+              kGot[i].is_padding_for_first_passed_data_model)
+        << "i = " << i;
+  }
+}
+
+TEST(UniquePadding, Std16BitDataModelToStd32BitDataModel) {
+  struct TestedStructType {
+    uint8_t a;
+    int32_t b;
+    int8_t c;
+    uint8_t d;
+  };
+  using TestedFirstDataModel = test_data::data_models::Simple16BitDataModel;
+  using TestedSecondDataModel = test_data::data_models::Simple32BitDataModel;
+
+  constexpr std::array<little_pp::unique_padding_element::UniquePaddingElement,
+                       2>
+      kExpected = {
+          little_pp::unique_padding_element::UniquePaddingElement{2, 2, false},
+          little_pp::unique_padding_element::UniquePaddingElement{10, 2, false},
+      };
+
+  auto constexpr kGot = little_pp::padding_reflection::unique_padding_v<
+      TestedStructType, TestedFirstDataModel, TestedSecondDataModel>;
+
+  EXPECT_EQ(kExpected.size(), kGot.size());
+  for (std::size_t i = 0; i < kExpected.size(); i++) {
+    EXPECT_EQ(kExpected[i].padding_first_index, kGot[i].padding_first_index)
+        << "i = " << i;
+    EXPECT_EQ(kExpected[i].padding_span_length, kGot[i].padding_span_length)
+        << "i = " << i;
+    EXPECT_EQ(kExpected[i].is_padding_for_first_passed_data_model,
+              kGot[i].is_padding_for_first_passed_data_model)
+        << "i = " << i;
   }
 }
